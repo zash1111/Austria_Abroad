971376ef74b7548c9377fcdc10120a584fc5fe64
canjohnb <candeniz@bayraktars.com>
2026-07-06 08:47:45 -0400

    Updated Itinerary.java
    REMOVED:
    - Various comments

--- Code/Itinerary.java
+++ Code/Itinerary.java
@@ -1,16 +1,11 @@
 import java.util.*;
 public class Itinerary extends Event {
-  /*
-  Event objects should each have a destination (String), date (month, day, year [Integer array]), time (hours, 
-  minutes [Integer array]), cost (double), and other fields...
-  */
   private ArrayList<Event> events;
   private double cost = 0.0;
 
   public Itinerary() {
     this.events = new ArrayList<Event>();
     for (Event e : events) {
-      // NOTE: getCost() method in the Event class must return the cost of the event!!
       this.cost += e.getCost();
     }
   }
@@ -46,7 +41,5 @@ public class Itinerary extends Event {
     events = events.clear();
     cost = 0.0;
   }
-
-  
     
 }
